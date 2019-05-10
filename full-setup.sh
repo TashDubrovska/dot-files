5349560230d2f4396d87907f661cd13b5ae0fee5
TashDubrovska <n.dubrovska89@gmail.com>
2019-05-10 11:53:38 +0100

    More fixes and attempt to better source bash config

--- full-setup.sh
+++ full-setup.sh
@@ -1,10 +1,8 @@
+./bash/configure.sh && (
 ./dependancies/install.sh
 ./node/install.sh
 ./vscode/install-and-configure.sh
 ./ubuntu/configure-dock.sh
 
-# Fix anything broken
-apt --fix-broken install
-
 ./ubuntu/set-wallpaper.sh
-./git/ssh-key.sh
+./git/ssh-key.sh)
--- node/install.sh
+++ node/install.sh
@@ -5,7 +5,6 @@ sudo apt -f -y install
 
 # Install NVM
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-./bash/configure.sh && 
 nvm install 10.13.0
 nvm use 10.13.0
 sudo npm install -g avn
